ebb48afded1a40cc4de5ff837d1c423ca5db374c
Andrey_Tarasov <lacryk@gmail.com>
2024-07-23 09:28:40 +0300

    some fixes

--- src/utils.py
+++ src/utils.py
@@ -8,9 +8,9 @@ from data.config_path import json_path
 ROOT_Path = Path(__file__).resolve().parent.parent
 
 
-def read_json(path: str) -> dict:
+def read_json() -> dict:
     """Чтение файла json"""
-    with open(Path(ROOT_Path,json_path), "r", encoding="utf-8") as file:
+    with open(Path(ROOT_Path, json_path), "r", encoding="utf-8") as file:
         data = json.load(file)
         return data
 
@@ -18,7 +18,7 @@ def read_json(path: str) -> dict:
 def create_objects_from_json(data):
     """Преобразование элементов списка словарей в объекты класса Product, Category"""
     categories = []
-    for category in list_products:
+    for category in data:
         products = []
         for product in category["products"]:
             products.append(Product(**product))
@@ -28,5 +28,5 @@ def create_objects_from_json(data):
     return categories
 
 
-list_products = read_json(json_path)
+list_products = read_json()
 list_categories = create_objects_from_json(list_products)
