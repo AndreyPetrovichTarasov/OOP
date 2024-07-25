dc2e0a3fb597deff7150fbfc9e41a81c58a72279
Andrey_Tarasov <lacryk@gmail.com>
2024-07-25 13:56:06 +0300

    tasks 1-4

--- src/product.py
+++ src/product.py
@@ -7,5 +7,25 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product_info: dict):
+        return cls(
+            name=product_info.get('name'),
+            description=product_info.get('description'),
+            price=product_info.get('price'),
+            quantity=product_info.get('quantity')
+        )
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        if value <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = value
--- tests/category.py
+++ tests/category.py
@@ -1,3 +1,7 @@
+from src.product import Product
+from src.category import Category
+
+
 def test_category(category):
     assert category.name == "Смартфоны"
     assert category.description == "Смартфоны, как средство"
@@ -5,3 +9,34 @@ def test_category(category):
 
     assert category.product_count == 0
     assert category.category_count == 1
+
+
+def test_add_product():
+    # Arrange
+    category = Category("Electronics", "Various electronic products", [])
+    product = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+
+    # Act
+    category.add_product(product)
+
+    # Assert
+    assert category.product_count == 1
+    assert category.products == 'Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n'
+
+
+def test_products_property():
+    # Arrange
+    category = Category("Electronics", "Various electronic products")
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("iPhone 14", "128GB, Black", 150000.0, 3)
+    category.add_product(product1)
+    category.add_product(product2)
+
+    # Act
+    products_str = category.products
+
+    # Assert
+    assert "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт." in products_str
+    assert "iPhone 14, 150000.0 руб. Остаток: 3 шт." in products_str
+    assert "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n" in products_str
+    assert "iPhone 14, 150000.0 руб. Остаток: 3 шт.\n" in products_str
